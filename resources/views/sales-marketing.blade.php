4345634446ba66d53a61edf3433484d354e93d61
Christineart <e88292774@gmail.com>
2026-04-22 15:31:10 +0800

    remove releases pill from sales-marketing banner

--- resources/views/sales-marketing.blade.php
+++ resources/views/sales-marketing.blade.php
@@ -20,10 +20,6 @@
                 <svg width="13" height="13" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/></svg>
                 {{ $todayTrips }} Tripping{{ $todayTrips != 1 ? 's' : '' }} Today
             </span>
-            <span class="today-pill {{ $todayReleases > 0 ? 'today-pill-alert' : '' }}">
-                <svg width="13" height="13" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>
-                {{ $todayReleases }} Release{{ $todayReleases != 1 ? 's' : '' }} Today
-            </span>
         </div>
     </div>
     <div class="welcome-decoration">
