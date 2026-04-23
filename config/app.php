222ac22f5d5c846cdb63c42aaf737152986ab8cd
Christineart <e88292774@gmail.com>
2026-04-23 15:03:12 +0800

    fix syntax error in config/app.php

--- config/app.php
+++ config/app.php
@@ -4,14 +4,9 @@ return [
 
     'scheduler_secret' => env('SCHEDULER_SECRET'),
 
-
+    /*
     |--------------------------------------------------------------------------
-    |
-    | This value is the name of your application, which will be used when the
-    | framework needs to place the application's name in a notification or
-    | other UI elements where an application name needs to be displayed.
-    |
-    */
+    | Application Name
 
     'name' => env('APP_NAME', 'Laravel'),
 
